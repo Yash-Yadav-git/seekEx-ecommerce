fe5ea4ad0a00c0cde126bb23f202647a8fdbb035
Yash Yadav <yashyadavgit@gmail.com>
2023-08-26 17:10:40 +0530

    hover functionality added

--- src/components/common-components/Card.jsx
+++ src/components/common-components/Card.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./card.css";
 import image1 from "../../static/card-component/image1.png";
+import shareIcon from "../../static/card-component/share_vector.png";
+import wishlistIcon from "../../static/card-component/wishlist_vector.png";
+import cartIcon from "../../static/card-component/cart_vector.png";
 
 const Card = () => {
   return (
     <article class="card">
-      <img src={image1} alt="Product Image" class="card-image" />
+      <div className="card-image-wrapper">
+        <img src={image1} alt="Product Image" class="card-image" />
+        <div className="produt-cta-wrapper">
+          <div className="prouct-cta">
+            <img src={cartIcon} alt="Product Cta" class="product-cta-icon" />
+          </div>
+          <div className="prouct-cta">
+            <img src={wishlistIcon} alt="Product Cta" class="product-cta-icon" />
+          </div>
+          <div className="prouct-cta">
+            <img src={shareIcon} alt="Product Cta" class="product-cta-icon" />
+          </div>
+        </div>
+      </div>
       <div class="card-description">
         <span class="description1">Men Henley Neck</span>
         <span class="description1">Full Sleeve Red Wine 1</span>
